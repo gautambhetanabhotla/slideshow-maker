5ea7f88bba9ffdf72f4e60fe3815d93d3380b9a1
gautambhetanabhotla <gautam.bhetanabhotla@students.iiit.ac.in>
2024-02-20 14:35:41 +0530

    minor commit
    sorry for this

--- app.py
+++ app.py
@@ -17,13 +17,14 @@ connection = pymysql.connect(host='localhost', user='gautam', password='haha')
 db = connection.cursor(pymysql.cursors.DictCursor)
 db.execute("CREATE DATABASE IF NOT EXISTS existentia")
 db.execute("USE existentia")
-db.execute("CREATE TABLE IF NOT EXISTS users(username VARCHAR(255), password VARCHAR(64), email VARCHAR(255), PRIMARY KEY(username))")
+db.execute("CREATE TABLE IF NOT EXISTS users(name VARCHAR(255), username VARCHAR(255), password VARCHAR(255), email VARCHAR(255), PRIMARY KEY(username))")
 db.execute("CREATE TABLE IF NOT EXISTS images(username VARCHAR(255), image_id INT, image BLOB, FOREIGN KEY(username) REFERENCES users(username))")
 db.execute("CREATE TABLE IF NOT EXISTS audios(audio BLOB, username VARCHAR(255), FOREIGN KEY(username) REFERENCES users(username))")
 connection.commit()
 
 app = Flask(__name__)
-users = json.load(open("data/users.json"))
+db.execute("SELECT * FROM users")
+users = db.fetchall()
 if os.path.exists("./uploads"):
 	app.config['UPLOAD_FOLDER'] = "./uploads"
 else:
@@ -56,31 +57,27 @@ def processloginrequest():
 		username = request.form["username"]
 		password = request.form["password"]
 		for user in users:
-			if user["username"] == username and user["password"] == password:
+			if user["username"] == username and user["password"] == hashed(password):
 				if username != "admin":
 					return redirect("/home", 301)
 				else:
-					return redirect("/admin", 301)
+					return user["password"] + hashed(password)
 		else:
 			return redirect("/login", 301)
 		
 @app.route("/requestsignup", methods = ['POST'])
 def processsignuprequest():
 	if request.method == 'POST':
-		payload = {
-			"username": request.form["username"],
-			"password": request.form["password"]
-		}
-		# token = jwt.encode(payload, "_SECRET_KEY_EXISTENTIA_", algorithm = "HS256")
-		# return token
 		username = request.form["username"]
 		password = request.form["password"]
+		email = request.form["email"]
+		name = request.form["name"]
 		for user in users:
 			if user["username"] == username:
 				return redirect("/signup", 301)
 		else:
-			users.append({"username": username, "password": password})
-			json.dump(users, open("data/users.json", "w"))
+			db.execute("INSERT INTO users VALUES(%s, %s, %s, %s)", (name, username, hashed(password), email))
+			connection.commit()
 			return redirect("/login", 301)
 
 @app.route("/admin")
